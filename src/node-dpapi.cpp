ade18910c31572f47ae18c7d326db6ca2d249af7
justin.li <justin.li@lungteh.com>
2025-08-13 18:55:52 +0800

    fix C2664 const char build error

--- src/node-dpapi.cpp
+++ src/node-dpapi.cpp
@@ -4,7 +4,7 @@
 #include <dpapi.h>
 #include <functional>
 
-v8::Local<v8::String> CreateUtf8String(v8::Isolate* isolate, char* strData)
+v8::Local<v8::String> CreateUtf8String(v8::Isolate* isolate, const char* strData)
 {
 	return v8::String::NewFromUtf8(isolate, strData, v8::NewStringType::kNormal).ToLocalChecked();
 }
